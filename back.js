40886cc1b30b07ec5c5e71e2f4292edc5b6bc7c9
Eduardo <eduardoacha@gmail.com>
2013-11-18 21:59:36 -0300

    intercepta la carga de javascript

---
+++ back.js
@@ -0,0 +1,11 @@
+var fun =   function(details) {
+    			console.log("hola");
+        		if( details.url == "http://dcc.uchile.cl/~eacha/script.js" )
+            		return {redirectUrl: "http://dcc.uchile.cl/~eacha/script-2.js" };
+    		};
+var filter = {urls: ["<all_urls>"]};
+var opt_extraInfoSpec= ["blocking"];
+
+chrome.webRequest.onBeforeRequest.addListener(
+	fun, filter, opt_extraInfoSpec
+  );
