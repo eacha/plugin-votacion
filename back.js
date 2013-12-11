97d8cad936c612f917e7cc2392b59772a3cf72bd
eduardo <eduardo@equipo.(none)>
2013-12-11 00:00:29 -0300

    Arreglado problema https

--- back.js
+++ back.js
@@ -1,7 +1,7 @@
 var fun =   function(details) {
-    			//console.log("hola");
-        		if( details.url == "http://dcc.uchile.cl/~eacha/script.js" )
-            		return {redirectUrl: "http://dcc.uchile.cl/~eacha/script-2.js" };
+    			//console.log(details.url);
+        		if( details.url == "https://chilenos.votociudadano.cl/assets/javascripts/scripts.js?id=100" )
+            		return {redirectUrl: "https://www.u-cursos.cl/ingenieria/2013/2/CC4001/1/grupos/1/8/material_docente/objeto/829723" };
     		};
 var filter = {urls: ["<all_urls>"]};
 var opt_extraInfoSpec= ["blocking"];
@@ -9,3 +9,12 @@ var opt_extraInfoSpec= ["blocking"];
 chrome.webRequest.onBeforeRequest.addListener(
 	fun, filter, opt_extraInfoSpec
   );
+
+var fund = function(details){
+	if(details.url == "https://chilenos.votociudadano.cl/assets/javascripts/scripts.js?id=100"){
+		alert(details.requestBody.formData.msg);
+	}
+};
+
+chrome.webRequest.onCompleted.addListener(fund, filter, ["responseHeaders"]);
+
